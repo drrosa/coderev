de1f6b6ea004b1918fac69def72a962ef7951a2c
David Rosa <rosatamsen@gmail.com>
2024-05-20 18:02:54 -0400

    files: Implement client-side create functionality.

---
+++ src/components/NewFileForm/NewFileForm.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+
+export default function NewFileForm({ addFile, user }) {
+  const [content, setContent] = useState('');
+  const [filename, setFilename] = useState('');
+
+  async function handleAddFile(evt) {
+    evt.preventDefault();
+    const fileData = { filename, content, user };
+    addFile(fileData);
+    setFilename(filename);
+    setContent(content);
+  }
+
+  return (
+    <div className="new-file-form">
+      <h2>New File</h2>
+      <form onSubmit={handleAddFile}>
+        <input
+          value={filename}
+          onChange={(evt) => setFilename(evt.target.value)}
+          placeholder="Filename"
+          required
+          pattern=".{4,}"
+        />
+        <textarea
+          value={content}
+          onChange={(evt) => setContent(evt.target.value)}
+          placeholder="Content..."
+          required
+          pattern=".{4,}"
+        />
+        <button type="submit">SAVE FILE</button>
+      </form>
+    </div>
+  );
+}
