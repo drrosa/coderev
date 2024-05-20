dd3e7b419437b6b69331d4606594c247a940051d
David Rosa <rosatamsen@gmail.com>
2024-05-20 14:57:00 -0400

    cleanup: Remove unnecessary pages/components/functions/routes.

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,7 +2,6 @@ const express = require('express');
 
 const router = express.Router();
 const usersCtrl = require('../../controllers/api/users');
-const ensureLoggedIn = require('../../config/ensureLoggedIn');
 
 // All paths start with '/api/users'
 
@@ -10,7 +9,5 @@ const ensureLoggedIn = require('../../config/ensureLoggedIn');
 router.post('/', usersCtrl.create);
 // POST /api/users/login
 router.post('/login', usersCtrl.login);
-
-router.get('/check-token', ensureLoggedIn, usersCtrl.checkToken);
 
 module.exports = router;
