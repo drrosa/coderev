c42acaffe77fb70c7c83e1748fcbb1560f0bcba0
Allan Viray <allang.v.dev@gmail.com>
2024-05-20 20:22:13 -0500

    Implement JS language support and apply theme to Editor

--- src/components/Editor/Editor.jsx
+++ src/components/Editor/Editor.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import CodeMirror from '@uiw/react-codemirror';
+import { javascript } from '@codemirror/lang-javascript';
+import { tokyoNight } from '@uiw/codemirror-theme-tokyo-night';
 
 export default function Editor() {
   const [text, setText] = useState('');
@@ -8,6 +10,7 @@ export default function Editor() {
       <CodeMirror
         value={text}
         onChange={(value) => setText(value)}
+        theme={tokyoNight}
         height="300px"
         style={{ textAlign: 'left' }}
         options={{
@@ -15,6 +18,7 @@ export default function Editor() {
           tabSize: 2,
           indentWithTabs: true,
         }}
+        extensions={[javascript({ jsx: true })]}
       />
     </>
   );
