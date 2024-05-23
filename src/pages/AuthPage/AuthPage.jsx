652c37cfacc0bea50c38d8146c22112fe1a5f2ed
Allan Viray <allang.v.dev@gmail.com>
2024-05-23 16:29:16 -0500

    Integrate React Spectrum's Form, TextField, and Button components in LoginForm

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -5,13 +5,13 @@ import LoginForm from '../../components/LoginForm/LoginForm';
 export default function AuthPage({ setUser }) {
   const [showSignUp, setShowSignUp] = useState(false);
   return (
-    <main>
+    <>
       <h1>AuthPage</h1>
       <button onClick={() => setShowSignUp(!showSignUp)}>{showSignUp ? 'Log In' : 'Sign Up'}</button>
       { showSignUp
         ? <SignUpForm setUser={setUser} />
         : <LoginForm setUser={setUser} />
       }
-    </main>
+    </>
   );
 }
