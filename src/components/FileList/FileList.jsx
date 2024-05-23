f35bce2d573e6e6d1d8caa622aca6dcd3965578a
DaveTheWay1 <ramirezvazquez826@gmail.com>
2024-05-23 15:54:15 -0700

    add some styling to the main view

--- src/components/FileList/FileList.jsx
+++ src/components/FileList/FileList.jsx
@@ -1,12 +1,13 @@
 import FileItem from '../FileItem/FileItem';
+import './FileList.css';
 
 export default function FileList({ files, findFile }) {
   // eslint-disable-next-line max-len
   const fileListItems = files.map((file, idx) => <FileItem file={file} key={idx} findFile={findFile} />);
   return (
     <>
-      <h1>Files</h1>
-      <ul>{fileListItems}</ul>
+      <h1>File Versions</h1>
+      <ul className='file-list' >{fileListItems}</ul>
     </>
   );
 }
