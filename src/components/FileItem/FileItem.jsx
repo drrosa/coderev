cafdbc3762f93b9b587760a5b96b75d59b9f0cc7
David Rosa <rosatamsen@gmail.com>
2024-05-23 12:36:14 -0400

    cleanup: Remove commented out code and add linter rule exceptions.

--- src/components/FileItem/FileItem.jsx
+++ src/components/FileItem/FileItem.jsx
@@ -1,3 +1,4 @@
 export default function FileItem({ file, findFile }) {
+  // eslint-disable-next-line no-underscore-dangle
   return <li><div onClick={() => findFile(file._id)}>{file.filename}</div></li>;
 }
