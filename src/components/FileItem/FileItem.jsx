a7aa7d2f7b1618e60fab4353494e36130b52a8d5
David Rosa <rosatamsen@gmail.com>
2024-05-23 20:10:20 -0400

    files: Add timestamps to the file list.

--- src/components/FileItem/FileItem.jsx
+++ src/components/FileItem/FileItem.jsx
@@ -1,6 +1,20 @@
 import './FileItem.css';
 
+function getTimeStamp(file) {
+  const dateTime = new Date(file.createdAt);
+  const options = {
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: true,
+  };
+  const formattedDate = new Intl.DateTimeFormat('en-US', options).format(dateTime);
+  return formattedDate;
+}
+
 export default function FileItem({ file, findFile }) {
   // eslint-disable-next-line no-underscore-dangle
-  return <li className="file"><div onClick={() => findFile(file._id)}>{file.filename}</div></li>;
+  return <li className="file"><div onClick={() => findFile(file._id)}>{file.filename} - {getTimeStamp(file)}</div></li>;
 }
