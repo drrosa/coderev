3b97f83c7e44d78b461ed27b902d6672060949af
David Rosa <rosatamsen@gmail.com>
2024-05-23 23:01:20 -0400

    style: Render the file list using Spectrum and adjust positioning to the left.

--- src/components/FileList/FileList.jsx
+++ src/components/FileList/FileList.jsx
@@ -1,13 +1,39 @@
-import FileItem from '../FileItem/FileItem';
+import { ListView, Item } from '@adobe/react-spectrum';
 import './FileList.css';
 
+function getTimeStamp(file) {
+  const dateTime = new Date(file.createdAt);
+  const options = {
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: true,
+  };
+  const formattedDate = new Intl.DateTimeFormat('en-US', options).format(dateTime);
+  return formattedDate;
+}
+
 export default function FileList({ files, findFile }) {
-  // eslint-disable-next-line max-len
-  const fileListItems = files.map((file, idx) => <FileItem file={file} key={idx} findFile={findFile} />);
   return (
-    <>
+    <div className="file-list-container">
       <h1>File Versions</h1>
-      <ul className='file-list' >{fileListItems}</ul>
-    </>
+      <ListView
+        selectionMode="none"
+        aria-label="File List"
+        maxWidth="size-5000"
+        onAction={(key) => {
+          findFile(key);
+        }}
+      >
+        {files.map((file) => (
+          // eslint-disable-next-line no-underscore-dangle
+          <Item key={file._id} textValue={file.filename}>
+            {`${file.filename} (${getTimeStamp(file)})`}
+          </Item>
+        ))}
+      </ListView>
+    </div>
   );
 }
